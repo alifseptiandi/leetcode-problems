4f2829bdb714e7f3a11431a551cc8e684a074584
septiandyy <17210164@bsi.ac.id>
2024-09-14 09:56:50 +0700

    submission completed

---
+++ 2. Add Two Numbers/2. Add Two Numbers.ts
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function addTwoNumbers (l1: ListNode | null, l2: ListNode | null): ListNode | null {
+    const dummyHead: ListNode = new ListNode(0);
+    let curr: ListNode = dummyHead;
+    let carry: number = 0;
+    
+    while (l1 !== null || l2 !== null || carry !== 0) {
+        const x: number = l1 ? l1.val : 0;
+        const y: number = l2 ? l2.val : 0;
+        const sum: number = carry + x + y;
+        carry = Math.floor(sum / 10);
+        curr.next = new ListNode(sum % 10);
+        curr = curr.next;
+        
+        if (l1) l1 = l1.next;   
+        if (l2) l2 = l2.next;
+    }
+    
+    return dummyHead.next;
+}
